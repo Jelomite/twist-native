52f0404e2a1d0100c149df5d403d4d3e24abb1df
suerflowz <moshesher1998@gmail.com>
2018-10-14 14:27:33 +0300

    replace method of using props

--- src/components/list.js
+++ src/components/list.js
@@ -3,25 +3,16 @@ import {FlatList, Text, View} from "react-native";
 import {ListItem} from "react-native-elements";
 
 class List extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: props.data,
-      style: props.style,
-      onPress: props.onPress
-    };
-  }
-
   render() {
     return (
       <View>
         <FlatList
           keyExtractor={(item, index) => index.toString()}
-          data={this.state.data}
+          data={this.props.data}
           renderItem={({item}: any) => (
             <ListItem
-              containerStyle={this.state.style.containerStyle}
-              titleStyle={this.state.style.titleStyle}
+              containerStyle={this.props.style.containerStyle}
+              titleStyle={this.props.style.titleStyle}
               title={item.title}
             />
           )}
