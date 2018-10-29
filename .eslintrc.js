077fe3d96fc655ca096268a3caf13ff01298b905
suerflowz <moshesher1998@gmail.com>
2018-10-29 18:22:18 +0200

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  globals: {
+    React: true
+  },
+
+  ecmaFeatures: {
+    jsx: true
+  },
+
+  plugins: ["react"],
+  rules: {
+    indent: ["error", "tab"],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "no-unused-vars": [2, {"vars": "all", "args": "after-used"}],
+    "react/jsx-uses-react": 1,
+    "react/jsx-uses-vars": 1,
+    "react/react-in-jsx-scope": 1,
+
+
+  }
+};
